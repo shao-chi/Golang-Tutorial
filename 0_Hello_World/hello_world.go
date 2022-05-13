81269c9c7862bf7f6149e225108f4a77f3c5b69e
jojo <shaochi1225@gmail.com>
2022-05-14 02:40:31 +0800

    feat: add time package

--- 0_Hello_World/hello_world.go
+++ 0_Hello_World/hello_world.go
@@ -1,8 +1,12 @@
 // 宣告程式屬於哪個 package
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, 世界")
+	fmt.Println("Hello, world")
+	fmt.Println("Time: ", time.Now())
 }
